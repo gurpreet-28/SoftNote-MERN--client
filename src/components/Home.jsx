32538ba56142813b31e70c8bbb7b022326134cf8
Jimmy <cynghjimmy0227@gmail.com>
2023-01-20 21:05:19 +0530

    fixed sigup bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import Form from "./Form";
 
 function Home(props) {
-  let navigate = useNavigate();
-
   return (
     <>
       <Form showAlert={props.showAlert} />
--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -4,13 +4,12 @@ import noteContext from "../context/notes/noteContext";
 function NoteItem(props) {
   const context = useContext(noteContext);
   const { deleteNote } = context;
-
   const { note, updateNote } = props;
+
   return (
     <>
       <div className="col-md-3">
-        <div className="card text-center  my-3">
-          <div className="card-header">User's Note</div>
+        <div className="card text-center my-3">
           <div className="card-body">
             <h5 className="card-title">{note.title}</h5>
             <p className="card-text">{note.description}</p>
